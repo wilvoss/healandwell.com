2dfd246bacbdbc7ebf2f5c35052e2e6d04ff14a9
Wil <wilvoss@me.com>
2019-03-31 13:57:40 -0700

    slight revert to working copy

--- assets/utilities.js
+++ assets/utilities.js
@@ -2,12 +2,14 @@ var body = document.body;
 
 function log(text, isolate) {
   if (isolate) {
-    console.log("...................................................");
+    console.log('...................................................');
   }
   console.log(text);
   if (isolate) {
-    console.log("Date: \t\t" + new Date(Date.now()).toLocaleDateString("en-US"));
-    console.log("...................................................");
+    console.log(
+      'Date: \t\t' + new Date(Date.now()).toLocaleDateString('en-US'),
+    );
+    console.log('...................................................');
   }
 }
 
@@ -17,7 +19,7 @@ function reloadPage() {
 
 function isValidEmailAddress(emailAddress) {
   var pattern = new RegExp(
-    /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i
+    /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
   );
   return pattern.test(emailAddress);
 }
@@ -28,15 +30,21 @@ function isValidPhoneNumber(phoneNumber) {
 }
 
 function isiOS() {
-  return navigator.platform.indexOf("iPad") != -1 || navigator.platform.indexOf("iPhone") != -1 || navigator.platform.indexOf("iPod") != -1;
+  return (
+    navigator.platform.indexOf('iPad') != -1 ||
+    navigator.platform.indexOf('iPhone') != -1 ||
+    navigator.platform.indexOf('iPod') != -1
+  );
 }
 
 function getUrlVars() {
   var vars = [],
     hash;
-  var hashes = window.location.href.slice(window.location.href.indexOf("?") + 1).split("&");
+  var hashes = window.location.href
+    .slice(window.location.href.indexOf('?') + 1)
+    .split('&');
   for (var i = 0; i < hashes.length; i++) {
-    hash = hashes[i].split("=");
+    hash = hashes[i].split('=');
     vars.push(hash[0]);
     vars[hash[0]] = hash[1];
   }
@@ -44,7 +52,7 @@ function getUrlVars() {
 }
 
 function getHashValue(key) {
-  var matches = location.hash.match(new RegExp(key + "=([^&]*)"));
+  var matches = location.hash.match(new RegExp(key + '=([^&]*)'));
   return matches ? matches[1] : null;
 }
 
@@ -54,7 +62,7 @@ function getRandomInt(min, max) {
 
 function commaSeparateNumber(val) {
   while (/(\d+)(\d{3})/.test(val.toString())) {
-    val = val.toString().replace(/(\d+)(\d{3})/, "$1" + "," + "$2");
+    val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ',' + '$2');
   }
   return val;
 }
--- controller/app_controller.js
+++ controller/app_controller.js
@@ -2,27 +2,23 @@ var app;
 var scope;
 
 var updateTime;
-var header = document.getElementsByTagName('header')[0];
-var nav = document.getElementsByTagName('nav')[0];
-var main = document.getElementsByTagName('main')[0];
-var mainBorderTop = parseInt(
-  $(main)
-    .css('border-top-width')
-    .replace('px', ''),
-);
-var footer = document.getElementsByTagName('footer')[0];
-var navList = document.getElementById('navlist');
-var list = nav.getElementsByTagName('li');
+var header = document.getElementsByTagName("header")[0];
+var nav = document.getElementsByTagName("nav")[0];
+var main = document.getElementsByTagName("main")[0];
+var mainBorderTop = parseInt($(main).css("border-top-width").replace("px", ""));
+var footer = document.getElementsByTagName("footer")[0];
+var navList = document.getElementById("navlist");
+var list = nav.getElementsByTagName("li");
 var listWidth = null;
 var listHeight = null;
 var delta = null;
 var narrow = false;
 
-angular.module('appModule', []);
+angular.module("appModule", []);
 
 angular
-  .module('appModule')
-  .controller('appController', function($timeout, $scope) {
+  .module("appModule")
+  .controller("appController", function ($timeout, $scope) {
     app = this;
     scope = angular.element($(document.body)).scope();
 
@@ -31,30 +27,25 @@ angular
     app.IsNarrow = false;
     app.MenuOn = false;
     app.IsModernTheme = false;
-    app.SectionWidth = 1048;
-    app.SectionOffset = 0;
 
     //    document.getElementById("mainCss").href = app.IsModernTheme ? "css/main-modern.css" : "css/main.css";
-    app.ToggleIsModernTheme = function() {
+    app.ToggleIsModernTheme = function () {
       app.IsModernTheme = !app.IsModernTheme;
-      document.getElementById('mainCss').href = app.IsModernTheme
-        ? 'css/main-modern.css'
-        : 'css/main.css';
+      document.getElementById("mainCss").href = app.IsModernTheme ? "css/main-modern.css" : "css/main.css";
     };
 
-    app.CurrentMainHeight = '0px';
-    //app.CurrentPageHeight = 'auto';
-    app.ContactGoogleMapCurrentHeight = '0px';
-    app.PreviousNarrowMapHeight = '0px';
+    app.CurrentMainHeight = "0px";
+    app.ContactGoogleMapCurrentHeight = "0px";
+    app.PreviousNarrowMapHeight = "0px";
     /* ============= Theme management ============= */
     app.CurrentTheme;
     app.Themes = {
-      coffee: new ThemeObject({ name: 'coffee' }),
-      blue: new ThemeObject({ name: 'blue' }),
-      blueLight: new ThemeObject({ name: 'blue light', isDisabled: false }),
-      orange: new ThemeObject({ name: 'orange', isDisabled: true }),
+      coffee: new ThemeObject({ name: "coffee" }),
+      blue: new ThemeObject({ name: "blue" }),
+      blueLight: new ThemeObject({ name: "blue light", isDisabled: false }),
+      orange: new ThemeObject({ name: "orange", isDisabled: true })
     };
-    app.SelectTheme = function(selected_theme) {
+    app.SelectTheme = function (selected_theme) {
       for (var theme in app.Themes) {
         app.Themes[theme].isSelected = false;
       }
@@ -67,17 +58,17 @@ angular
     /* ============= Font management ============= */
     app.CurrentFont;
     app.Fonts = {
-      sans: new ThemeObject({ name: 'sans-serif', isSelected: false }),
-      serif: new ThemeObject({ name: 'serif', isSelected: true }),
+      sans: new ThemeObject({ name: "sans-serif", isSelected: false }),
+      serif: new ThemeObject({ name: "serif", isSelected: true })
     };
 
     app.CurrentFontSize;
     app.FontSizes = {
-      normal: new ThemeObject({ name: 'normal', isSelected: true }),
-      big: new ThemeObject({ name: 'big', isSelected: false }),
-      bigger: new ThemeObject({ name: 'bigger', isSelected: false }),
+      normal: new ThemeObject({ name: "normal", isSelected: true }),
+      big: new ThemeObject({ name: "big", isSelected: false }),
+      bigger: new ThemeObject({ name: "bigger", isSelected: false })
     };
-    app.SelectFontSize = function(selected_font) {
+    app.SelectFontSize = function (selected_font) {
       for (var font in app.FontSizes) {
         app.FontSizes[font].isSelected = false;
       }
@@ -90,7 +81,7 @@ angular
     app.ExpandAllResources = false;
     app.ResourceGroups = BaseResourceGroups;
 
-    app.ToggleAllResources = function() {
+    app.ToggleAllResources = function () {
       app.ExpandAllResources = !app.ExpandAllResources;
       for (i = 0; i < app.ResourceGroups.length; i++) {
         var group = app.ResourceGroups[i];
@@ -101,74 +92,27 @@ angular
     /* ============= Navigation management ============= */
     app.CurrentLink;
     app.NavigationLinks = {
-      home: new NavigationLinkObject({
-        id: 'home',
-        name: 'Welcome',
-        position: 0,
-        page: 'pages/welcome.html',
-      }),
-      services: new NavigationLinkObject({
-        id: 'services',
-        name: 'Services',
-        position: 1,
-        page: 'pages/services.html',
-      }),
-      about: new NavigationLinkObject({
-        id: 'about',
-        name: 'About',
-        position: 2,
-        page: 'pages/about.html',
-      }),
-      connect: new NavigationLinkObject({
-        id: 'connect',
-        name: 'Connect',
-        position: 3,
-        page: 'pages/connect.html',
-        previousIds: ['contact'],
-      }),
-      newClient: new NavigationLinkObject({
-        id: 'newClient',
-        name: 'New clients',
-        position: 4,
-        page: 'pages/newClient.html',
-        previousId: ['intake'],
-      }),
-      resources: new NavigationLinkObject({
-        id: 'resources',
-        name: 'Resources',
-        position: 5,
-        page: 'pages/resources.html',
-      }),
+      home: new NavigationLinkObject({ id: "home", name: "Welcome", position: 0, page: "pages/welcome.html" }),
+      services: new NavigationLinkObject({ id: "services", name: "Services", position: 2, page: "pages/services.html" }),
+      about: new NavigationLinkObject({ id: "about", name: "About", position: 3, page: "pages/about.html" }),
+      connect: new NavigationLinkObject({ id: "connect", name: "Connect", position: 4, page: "pages/connect.html", previousIds: ["contact"] }),
+      newClient: new NavigationLinkObject({ id: "newClient", name: "New clients", position: 5, page: "pages/newClient.html", previousId: ["intake"] }),
+      resources: new NavigationLinkObject({ id: "resources", name: "Resources", position: 6, page: "pages/resources.html" })
     };
 
-    app.SelectLink = function(selected_link) {
-      narrow =
-        document.body.offsetWidth - delta < listWidth ||
-        document.body.offsetHeight < 550;
-      if (narrow) {
-        app.SectionWidth =
-          document.body.offsetWidth > 1048
-            ? 1048
-            : document.body.offsetWidth - 40;
-      }
+    app.SelectLink = function (selected_link) {
       for (var link in app.NavigationLinks) {
-        var target = app.NavigationLinks[link];
-        target.isSelected = false;
-        if (selected_link == target) {
-          var narrowAccomodation = narrow ? 40 : 0;
-          app.SectionOffset =
-            (-app.SectionWidth - narrowAccomodation) * target.position;
-        }
+        app.NavigationLinks[link].isSelected = false;
       }
       if (selected_link == app.NavigationLinks.connect) {
-        log('select link called');
+        log("select link called");
         //calculateCenter();
       }
       app.CurrentLink = selected_link;
       app.CurrentLink.isSelected = true;
     };
 
-    app.CheckHashAgainstPageIds = function(hash) {
+    app.CheckHashAgainstPageIds = function (hash) {
       for (link in app.NavigationLinks) {
         if (hash == app.NavigationLinks[link].id) {
           return hash;
@@ -181,10 +125,10 @@ angular
       return null;
     };
 
-    app.UpdateCheckURL = function(checkHash) {
+    app.UpdateCheckURL = function (checkHash) {
       checkHash = checkHash == undefined ? false : checkHash;
-      if (getHashValue('/page') != null) {
-        var pageId = app.CheckHashAgainstPageIds(getHashValue('/page'));
+      if (getHashValue("/page") != null) {
+        var pageId = app.CheckHashAgainstPageIds(getHashValue("/page"));
         if (checkHash == true) {
           if (pageId != null) {
             app.SelectLink(app.NavigationLinks[pageId]);
@@ -195,22 +139,22 @@ angular
           app.SelectLink(app.NavigationLinks[pageId]);
         }
       } else {
-        app.PushHistory('home');
+        app.PushHistory("home");
       }
     };
 
-    app.PushHistory = function(linkId) {
-      history.pushState(null, '', '#/page=' + linkId);
+    app.PushHistory = function (linkId) {
+      history.pushState(null, "", "#/page=" + linkId);
       app.UpdateCheckURL();
     };
   })
-  .filter('orderObjectBy', function() {
-    return function(items, field, reverse) {
+  .filter("orderObjectBy", function () {
+    return function (items, field, reverse) {
       var filtered = [];
-      angular.forEach(items, function(item) {
+      angular.forEach(items, function (item) {
         filtered.push(item);
       });
-      filtered.sort(function(a, b) {
+      filtered.sort(function (a, b) {
         return a[field] > b[field] ? 1 : -1;
       });
       if (reverse) filtered.reverse();
@@ -218,9 +162,9 @@ angular
     };
   });
 
-var NavigationLinkObject = function(spec) {
+var NavigationLinkObject = function (spec) {
   this.id = spec.id;
-  this.name = spec.name == undefined ? '' : spec.name;
+  this.name = spec.name == undefined ? "" : spec.name;
   this.position = spec.position;
   this.isSelected = spec.isSelected == undefined ? false : spec.isSelected;
   this.isDisabled = spec.isDisabled == undefined ? false : spec.isDisabled;
@@ -228,43 +172,32 @@ var NavigationLinkObject = function(spec) {
   this.page = spec.page;
 };
 
-var ThemeObject = function(spec) {
-  this.name = spec.name == undefined ? '' : spec.name;
+var ThemeObject = function (spec) {
+  this.name = spec.name == undefined ? "" : spec.name;
   this.position = spec.position;
   this.isSelected = spec.isSelected == undefined ? false : spec.isSelected;
   this.isDisabled = spec.isDisabled == undefined ? false : spec.isDisabled;
 };
 
-var FontObject = function(spec) {
-  this.name = spec.name == undefined ? '' : spec.name;
+var FontObject = function (spec) {
+  this.name = spec.name == undefined ? "" : spec.name;
   this.position = spec.position;
   this.isSelected = spec.isSelected == undefined ? false : spec.isSelected;
   this.isDisabled = spec.isDisabled == undefined ? false : spec.isDisabled;
 };
 
 function UpdateLayout() {
-  narrow =
-    document.body.offsetWidth - delta < listWidth ||
-    document.body.offsetHeight < 550;
+  narrow = document.body.offsetWidth - delta < listWidth || document.body.offsetHeight < 550;
   app.IsNarrow = narrow;
-  var newHeight =
-    document.body.offsetHeight -
-    header.offsetTop -
-    header.offsetHeight -
-    nav.offsetHeight -
-    footer.offsetHeight +
-    (app.IsNarrow ? 0 : 100);
-  //app.SectionWidth = document.body.offsetWidth > 1048 ? 1048 : document.body.offsetWidth - 40;
-
-  if (app.CurrentMainHeight != newHeight + 'px') {
+  var newHeight = document.body.offsetHeight - header.offsetTop - header.offsetHeight - nav.offsetHeight - footer.offsetHeight - (app.IsNarrow ? 0 : 20);
+  if (app.CurrentMainHeight != newHeight + "px") {
     // log(app.CurrentMainHeight + " != " + newHeight);
     if (app.IsNarrow) {
-      if (app.CurrentMainHeight != 'auto') {
-        app.CurrentMainHeight = 'auto';
-        app.CurrentPageHeight = newHeight + 147 + 'px';
+      if (app.CurrentMainHeight != "auto") {
+        app.CurrentMainHeight = "auto";
       }
-      if (app.PreviousNarrowMapHeight != newHeight + 'px') {
-        app.ContactGoogleMapCurrentHeight = parseInt(newHeight) + 'px';
+      if (app.PreviousNarrowMapHeight != (newHeight + "px")) {
+        app.ContactGoogleMapCurrentHeight = parseInt(newHeight) + "px";
         app.PreviousNarrowMapHeight = app.ContactGoogleMapCurrentHeight;
         if (app.NavigationLinks.connect.isSelected) {
           //log("calculateCenter()");
@@ -272,30 +205,25 @@ function UpdateLayout() {
         }
       }
     } else {
-      app.CurrentMainHeight = newHeight + 'px';
-      app.ContactGoogleMapCurrentHeight = parseInt(newHeight) - 80 + 'px';
+      app.CurrentMainHeight = newHeight + "px";
+      app.ContactGoogleMapCurrentHeight = parseInt(newHeight) - 80 + "px";
     }
   }
 
   if (narrow) {
-    var scrollTop = document.getElementsByTagName('wrap')[0].scrollTop;
+    var scrollTop = document.getElementsByTagName("wrap")[0].scrollTop;
     app.HealingAndWellnessScrolledOutOfView = scrollTop > 30;
   }
   scope.$apply();
 }
 
 function GetNavElementsWidth() {
-  mainBorderTop = parseInt(
-    $(main)
-      .css('border-top-width')
-      .replace('px', ''),
-  );
+  mainBorderTop = parseInt($(main).css("border-top-width").replace("px", ""));
   if (list.length > 0) {
     var width = 0;
-    var modifier = navigator.userAgent.indexOf('iPad') != -1 ? 73 : 0;
+    var modifier = navigator.userAgent.indexOf("iPad") != -1 ? 73 : 0;
     for (var i = list.length - 1; i >= 0; i--) {
-      width +=
-        list[i].getElementsByTagName('button')[0].offsetWidth + 2 - modifier;
+      width += list[i].getElementsByTagName("button")[0].offsetWidth + 2 - modifier;
     }
     listWidth = width;
     delta = 80; // navigator.userAgent.indexOf("iPad") != -1 ? 120 : navList.offsetWidth - listWidth;;
@@ -305,28 +233,25 @@ function GetNavElementsWidth() {
     app.UpdateCheckURL(true);
     scope.$apply();
     updateTime = window.setInterval(UpdateLayout, 16);
-    if (
-      document.getElementsByTagName('html')[0].className.indexOf('loaded') == -1
-    ) {
-      document.getElementsByTagName('html')[0].className =
-        document.getElementsByTagName('html')[0].className + ' loaded';
+    if (document.getElementsByTagName("html")[0].className.indexOf("loaded") == -1) {
+      document.getElementsByTagName("html")[0].className = document.getElementsByTagName("html")[0].className + " loaded";
     }
   } else {
     window.setTimeout(GetNavElementsWidth, 100);
   }
 }
 
-$(window).on('hashchange', function() {
+$(window).on("hashchange", function () {
   app.UpdateCheckURL(true);
   scope.$apply();
 });
 
-window.onpopstate = function(event) {
+window.onpopstate = function (event) {
   if (event && event.state) {
     window.location = window.location;
   }
 };
 
-document.body.onload = window.setTimeout(function() {
+document.body.onload = window.setTimeout(function () {
   GetNavElementsWidth();
 }, 100);
